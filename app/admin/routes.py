530dd80126b9a612a1491f172a6d03543431b280
manuchis <manuchis@gmail.com>
2019-04-09 17:36:05 +0200

    Added user-role edition in admin

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -11,6 +11,7 @@ from app.models import User, Post, Message, Notification, Company, Role, UserRol
 from app.translate import translate
 from app.main import bp
 from app.main.forms import SearchForm
+from app.admin.forms import EditProfileForm
 from app.admin import bp
 
 @bp.before_app_request
@@ -43,8 +44,35 @@ admin_permission = Permission(RoleNeed('admin'))
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
 @login_required
-#@admin_permission.required()
+@admin_permission.require()
 def admin():
     users = User.query.order_by(User.username.asc())
     companies = Company.query.order_by(Company.name.asc())
     return render_template('admin/index.html', title=_('Admin'), users=users, companies=companies)
+
+@bp.route('/user/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_permission.require()
+def admin_edit_user(id):
+
+    user = User.query.filter_by(id=id).first_or_404()
+    companies = Company.query.order_by(Company.name.asc())
+    form = EditProfileForm(user.username)
+    form.roles.choices = [(g.id, g.name) for g in Role.query.order_by('name')]
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.about_me = form.about_me.data
+        user.roles = Role.query.filter(Role.id.in_(form.roles.data)).all()
+        db.session.commit()
+        flash(_('Your changes have been saved.'))
+        return redirect(url_for('admin.admin_edit_user', id=id))
+    elif request.method == 'GET':
+        form.username.data = user.username
+        form.about_me.data = user.about_me
+        # pass roles id to list
+        rls= user.roles
+        rl = []
+        for role in rls:
+            rl.append(role.id)
+        form.roles.data = rl
+    return render_template('edit_profile.html', title=_('Edit user'), form=form)
